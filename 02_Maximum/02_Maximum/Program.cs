731e5cc4bc5a6e2540e81d9fa138041e3f941d2a
Турисов Роман Александрович <turisov-ra@psyhosp.lan>
2016-10-19 13:08:59 +0400

    Лекция 2. Пример 2. Максимальное из трех чисел через if

--- 02_Maximum/02_Maximum/Program.cs
+++ 02_Maximum/02_Maximum/Program.cs
@@ -8,23 +8,41 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int A, B;
-            if (int.TryParse(Console.ReadLine(), out A))
-            {
-                if (int.TryParse(Console.ReadLine(), out B))
+            int A, B, C;
+            if (int.TryParse(Console.ReadLine(), out A)&& int.TryParse(Console.ReadLine(), out B) && int.TryParse(Console.ReadLine(), out C))
                 {
                     //максимальное значение
                     int Max;
-                    //если больше А
-                    if (A >= B)
+                //если A >= B
+                if (A >= B)
+                {
+                    //и если A>= C
+                    if (A >= C)
                     {
                         Max = A;
                     }
-                    //иначе - больше В
+                    //иначе - C >= A
                     else
                     {
+                        Max = C;
+                    }
+                }
+                //иначе больше B
+                else
+                {
+                    //и если B >= C
+                    if (B >= C)
+                    {
+                        //то максимальное - B
                         Max = B;
-                    };
+                    }
+                    //иначе - C > B
+                    else
+                    {
+                        //максимальное C
+                        Max = C;
+                    }
+                };
                     //вывод результата
                     Console.WriteLine("Максимальное = " + Max);
                 }
@@ -32,11 +50,6 @@ namespace ConsoleApplication1
                 {
                     Console.WriteLine("Неверное значение! Нужно ввести целое число.");
                 };
-            }
-            else
-            {
-                Console.WriteLine("Неверное значение! Нужно ввести целое число.");
-            };
             //завершение программы
             Console.ReadKey();
         }
