bc9677cdaf82233390fce88464abddea786b40df
RomanTurisov <spidy1992@mail.ru>
2016-11-17 20:52:23 +0400

    Лекция 4. Пример 1. Фамилия, имя, отчество, если нет отчества

--- FIO/FIO/Program.cs
+++ FIO/FIO/Program.cs
@@ -15,25 +15,64 @@ namespace FIO
             Console.Write("Введите фамилию: ");
             string familiya = Console.ReadLine();
             //И. О. Фамилия
-            Console.WriteLine(imya[0].ToString().ToUpper() + ". "  //И.
-                + otchestvo[0].ToString().ToUpper() + ". "  //О.
-                + familiya[0].ToString().ToUpper()  //Ф
-                + familiya.Substring(1).ToLower());  //амилия
+            if (!string.IsNullOrEmpty(otchestvo))  //если отчество ввели
+            {
+                Console.WriteLine(imya[0].ToString().ToUpper() + ". "  //И.
+                    + otchestvo[0].ToString().ToUpper() + ". "  //О.
+                    + familiya[0].ToString().ToUpper()  //Ф
+                    + familiya.Substring(1).ToLower());  //амилия
+            }
+            else //иначе отчество не ввели
+            {
+                //выводим без отчетсва
+                Console.WriteLine(imya[0].ToString().ToUpper() + ". "  //И.
+                    + familiya[0].ToString().ToUpper()  //Ф
+                    + familiya.Substring(1).ToLower());  //амилия
+            };
             //Фамилия И. О.
-            Console.WriteLine(familiya[0].ToString().ToUpper()  //Ф
-                + familiya.Substring(1).ToLower() + " " //амилия
-                + imya[0].ToString().ToUpper() + ". "  //И.
-                + otchestvo[0].ToString().ToUpper() + ". ");  //О.
+            if (!string.IsNullOrEmpty(otchestvo))  //если отчество ввели
+            {
+                Console.WriteLine(familiya[0].ToString().ToUpper()  //Ф
+                    + familiya.Substring(1).ToLower() + " " //амилия
+                    + imya[0].ToString().ToUpper() + ". "  //И.
+                    + otchestvo[0].ToString().ToUpper() + ". ");  //О.
+            }
+            else  //иначе отчество не ввели
+            {
+                //выводим без отчетсва
+                Console.WriteLine(familiya[0].ToString().ToUpper()  //Ф
+                    + familiya.Substring(1).ToLower() + " " //амилия
+                    + imya[0].ToString().ToUpper() + ". ");  //И.
+            };
             //Имя Отчество ФАМИЛИЯ
-            Console.WriteLine(imya[0].ToString().ToUpper()  //И
-                + imya.Substring(1).ToLower() + " " //мя
-                + otchestvo[0].ToString().ToUpper()  //О
-                + otchestvo.Substring(1).ToLower() + " " //О.
-                + familiya.ToUpper());  //ФАМИЛИЯ
+            if (!string.IsNullOrEmpty(otchestvo))  //если отчество ввели
+            {
+                Console.WriteLine(imya[0].ToString().ToUpper()  //И
+                    + imya.Substring(1).ToLower() + " " //мя
+                    + otchestvo[0].ToString().ToUpper()  //О
+                    + otchestvo.Substring(1).ToLower() + " " //О.
+                    + familiya.ToUpper());  //ФАМИЛИЯ
+            }
+            else  //иначе отчество не ввели
+            {
+                //выводим без отчетсва
+                Console.WriteLine(imya[0].ToString().ToUpper()  //И
+                    + imya.Substring(1).ToLower() + " " //мя
+                    + familiya.ToUpper());  //ФАМИЛИЯ
+            };
             //фам_и_о
-            Console.WriteLine((familiya.Substring(0, 3) + "_"  //фам_
-                + imya.Substring(0, 1) + "_"  //и_
-                + otchestvo.Substring(0, 1)).ToLower());  //o
+            if (!string.IsNullOrEmpty(otchestvo))  //если отчество ввели
+            {
+                Console.WriteLine((familiya.Substring(0, 3) + "_"  //фам_
+                    + imya.Substring(0, 1) + "_"  //и_
+                    + otchestvo.Substring(0, 1)).ToLower());  //o
+            }
+            else  //иначе отчество не ввели
+            {
+                //выводим без отчетсва
+                Console.WriteLine((familiya.Substring(0, 3) + "_"  //фам_
+                    + imya.Substring(0, 1)));  //и_
+            };
             //закрытие программы
             Console.ReadKey();
         }
