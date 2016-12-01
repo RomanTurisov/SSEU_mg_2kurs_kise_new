8fc4111f950279828cec28d5d3efedcd40d4b76b
RomanTurisov <spidy1992@mail.ru>
2016-12-01 20:06:47 +0400

    Лекция 4. Пример 4. Сортировка слов по алфавиту

---
+++ SortAZ/SortAZ/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortAZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ввод строки
+            Console.WriteLine("Введите слова, разделенные пробелами:");
+            string words = Console.ReadLine();
+            //преобразование строки в массив, разделитель - пробел
+            string[] wordsArr = words.Split(' ');
+            //сортировка массива
+            Array.Sort(wordsArr);
+            //собираем массив обратно в строку
+            words = wordsArr[0];
+            for (int i = 1; i < wordsArr.Length; i++)
+                words = words + " " + wordsArr[i];
+            //вывод результата на экран
+            Console.WriteLine("Сортировка слов по алфавиту:");
+            Console.WriteLine(words);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SortAZ/SortAZ/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SortAZ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SortAZ")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c533ce2f-da9d-4f52-9dce-9735c6f7b143")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
